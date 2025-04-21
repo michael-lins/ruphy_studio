bd4dd277f030f9cfc15c3834c06d2db7777d3cec
Michael Lins <michael@malvins.studio>
2025-04-21 00:11:04 -0300

    clean up for ruphino show up

--- src/main.js
+++ src/main.js
@@ -1,18 +1,25 @@
-const { invoke } = window.__TAURI__.core;
+import { animate, spring } from "motion";
+import { shell } from "@tauri-apps/api/shell";
+import { resolveResource } from "@tauri-apps/api/path";
 
-let greetInputEl;
-let greetMsgEl;
+async function jumpIntoBrowser() {
+  // Animate Ruphino
+  animate("#ruphino", {
+    scale: [1, 1.2, 0],
+    opacity: [1, 1, 0],
+    y: [0, -30, -100]
+  }, {
+    duration: 1,
+    easing: spring({ stiffness: 200, damping: 20 })
+  });
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  // Let it finish
+  setTimeout(async () => {
+    const landingPath = await resolveResource("public/index.html");
+    await shell.open("file://" + landingPath);
+    window.close();
+  }, 1000);
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-});
+// Trigger the jump animation when the DOM content is loaded
+window.addEventListener("DOMContentLoaded", jumpIntoBrowser);
