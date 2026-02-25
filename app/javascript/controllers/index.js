4f6b5705d730317802d8ecfcbd04ad635b9dc547
Michael Lins <michael@malvins.studio>
2026-02-24 22:21:11 -0300

    cleaning up for poc in Golang for desktop app

--- app/javascript/controllers/index.js
+++
@@ -1,6 +0,0 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-
-// Automatically register all controllers
-eagerLoadControllersFrom("controllers", application)
